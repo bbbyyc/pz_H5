785661b30ea13913061672edc33bccf2320320a1
bbyyyc <2889161356@qq.com>
2026-03-11 23:02:04 +0800

    Initial commit: 初次上传项目

---
+++ src/pages/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>layout</p>
+  <router-view></router-view>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+// src/vite-env.d.ts
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
